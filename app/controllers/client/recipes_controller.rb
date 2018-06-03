f54e87b1396b2449a3ecb39ad05c1af464bd4c2f
dani <dani.zaghian@gmail.com>
2018-06-03 12:10:17 -0700

    added show action

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -1,7 +1,14 @@
 class Client::RecipesController < ApplicationController
+
 	def index
 		response = Unirest.get("http://localhost:3000/api/recipes/")
 		@recipes = response.body #all recipes in an array
 		render "index.html.erb"
+	end
+
+	def show
+		response = Unirest.get("http://localhost:3000/api/recipes/#{params[:id]}")
+		@recipe = response.body
+		render 'show.html.erb'
 	end
 end
