2a0ecc8b73815510c40193908cc188dcd9bbf90a
dani <dani.zaghian@gmail.com>
2018-06-12 18:22:14 -0700

    added login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :client do
-  	get "recipes" => "recipes#index"
+  	get "/recipes" => "recipes#index"
   	get "/recipes/new" => "recipes#new"
   	post "/recipes" => "recipes#create"
   	get "/recipes/:id" => "recipes#show"
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   	patch "/recipes/:id" => "recipes#update"
   	delete "/recipes/:id" => "recipes#destroy"
   end
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
 end
